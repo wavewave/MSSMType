884325ebc9e7611b733b3c13eabfb8366e9c2069
ianwookim <ianwookim@gmail.com>
2011-04-06 01:45:24 +0000

    add LesHouches.hs

---
+++ src/HEP/Physics/MSSM/LesHouches.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+
+module HEP.Physics.MSSM.LesHouches (
+  outputPhysToLesHouches
+  ) where 
+
+import Text.StringTemplate.Helpers 
+
+import HEP.Physics.MSSM.Type
+
+render :: String -> [(String,String)] -> String
+render = flip render1 
+
+pp = show
+
+outputPhysToLesHouches :: String -> OutputPhys -> String 
+outputPhysToLesHouches tmpl OutputPhys {..} = 
+  render tmpl [ ("mass_Mh"  , pp mass_Mh  )
+              , ("mass_MHH" , pp mass_MHH )
+              , ("mass_MH3" , pp mass_MH3 )
+              , ("mass_MHc" , pp mass_MHc )
+              , ("mass_MNE1", pp mass_MNE1)
+              , ("mass_MNE2", pp mass_MNE2)
+              , ("mass_MNE3", pp mass_MNE3)
+              , ("mass_MNE4", pp mass_MNE4)
+              , ("mass_MC1" , pp mass_MC1 )
+              , ("mass_MC2" , pp mass_MC2 )
+              , ("mass_MSG" , pp mass_MSG ) 
+              , ("mass_MSuL", pp mass_MSuL)
+              , ("mass_MSdL", pp mass_MSdL)
+              , ("mass_MSeL", pp mass_MSeL)
+              , ("mass_MSne", pp mass_MSne)
+              , ("mass_MSuR", pp mass_MSuR)
+              , ("mass_MSdR", pp mass_MSdR)
+              , ("mass_MSeR", pp mass_MSeR)
+              , ("mass_MScL", pp mass_MScL)
+              , ("mass_MSsL", pp mass_MSsL)
+              , ("mass_MSmL", pp mass_MSmL)
+              , ("mass_MSnm", pp mass_MSnm) 
+              , ("mass_MScR", pp mass_MScR)
+              , ("mass_MSsR", pp mass_MSsR)
+              , ("mass_MSmR", pp mass_MSmR)
+              , ("mass_MSt1", pp mass_MSt1)
+              , ("mass_MSb1", pp mass_MSb1)
+              , ("mass_MSl1", pp mass_MSl1)
+              , ("mass_MSn1", pp mass_MSn1)
+              , ("mass_MSt2", pp mass_MSt2)
+              , ("mass_MSb2", pp mass_MSb2)
+              , ("mass_MSl2", pp mass_MSl2) ] 
